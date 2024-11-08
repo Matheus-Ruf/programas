d3bbe215b9df815e85edc2a3da1517afef099d70
Matheus-Ruf <163956661+Matheus-Ruf@users.noreply.github.com>
2024-11-08 15:45:53 -0300

    Update jogo_da_velha.c

--- jogo_da_velha.c
+++ jogo_da_velha.c
@@ -1,71 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int venceu(int v[])//venceu == 1; nao venceu == 0;
+int verifica_velha(int v[])
 {
-	for(i = 0; i < 2; i++)//verifica de cada jogador
-	{
-		for(j = 0; j < 9; j = j + 3)//verifica as linhas
-		{
-			if(v[j] == i)
-			{
-				if(v[j+1] == i)
-				{
-					if(v[j+2] == i)
-						return 1;
-				}
-			}
-		}
-		
-		for(j = 0; j < 3; j++)//verifica as colunas
-		{
-			if(v[j] == i)
-			{
-				if(v[j+3] == i)
-				{
-					if(v[j+6] == i)
-						return 1;
-				}
-			}
-		}
-
-		for(j = 0; j < 3; j = j + 2)//verifica as diagonais
-		{
-			if( v[ 0 + j ] == i )
-			{
-				if( v[ 4 + (j-j) ] == i )
-				{
-					if( v[ 8 - j ] == i )
-						return 1;
-				}
-			}
-		}
-	}
-	return 0;
+	
 }
-
-//	[0|1|2]
-//	[3|4|5]
-//	[6|7|8]
 
 void imprime_tab(int v[])
 {
-	int i;
-	for(i = 0; i <= 8; i++)
-	{
-		if(i % 3 == 0)
-			printf("/t");
-		printf("[");
-		switch(v[i])
-		{
-			case 0:printf("O");break;
-			case 1:printf("X");break;
-			case 2:printf(" ");break;
-		}
-		printf("]");
-		if(i % 3 == 2)
-			printf("\n");
-	}
 	
 }
 
@@ -76,8 +18,8 @@ void adiciona_tab(int v[], int x)
 
 int main ()
 {
-	int v[2,2,2,2,2,2,2,2,2];//v[8]
-	int i, rodada, velha;
+	int v[8];
+	int i, rodada;
 	
 	while(1)
 	{
@@ -88,20 +30,21 @@ int main ()
 		if(i == 2)
 			return 0;
 		
+		velha = 0;
 		for(rodada = 1; rodada <= 9; rodada++)
 		{
-			imprime_tab();
-			if( venceu(v) )
+			if(velha == 1)
 			{
-				printf("GANHOU!!\n\n");
-				rodada = 9;
+				imprime_tab();
+				printf("GANHOU!!");
+				rodada = 10;
 			}
 			else
 			{
 				adiciona_tab(v, rodada % 2);
+				velha = verifica_velha(v);
 			}
 		}
-		if( !venceu(v) )
-			printf("VELHA!! o jogo acabou!\n\n");
+		printf("VELHA!! o jogo acabou!");
 	}
 }
