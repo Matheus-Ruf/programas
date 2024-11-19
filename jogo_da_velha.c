3df6586fba7c02214840b6c61e196b9a48e262c3
Matheus-Ruf <163956661+Matheus-Ruf@users.noreply.github.com>
2024-11-19 13:42:35 -0300

    Update jogo_da_velha.c

--- jogo_da_velha.c
+++ jogo_da_velha.c
@@ -3,6 +3,8 @@
 
 int venceu(int v[])//venceu == 1; nao venceu == 0;
 {
+	int i, j;
+	
 	for(i = 0; i < 2; i++)//verifica de cada jogador
 	{
 		for(j = 0; j < 9; j = j + 3)//verifica as linhas
@@ -52,7 +54,7 @@ void imprime_tab(int v[])
 	for(i = 0; i <= 8; i++)
 	{
 		if(i % 3 == 0)
-			printf("/t");
+			printf("\t");
 		printf("[");
 		switch(v[i])
 		{
@@ -64,43 +66,95 @@ void imprime_tab(int v[])
 		if(i % 3 == 2)
 			printf("\n");
 	}
-
+    return;
 }
 
-void adiciona_tab(int v[], int x)
+void imprime_tab_3(int v[], int i)
 {
+	for(int j = 0; j < 3; j++)
+		printf("[%d]", i + j + 1);
+	return;
+}
 
+void imprime_tab_opcoes(int v[])
+{
+    int i;
+    for(i = 0; i <= 8; i++)
+    {
+        if(i % 3 == 0)
+            imprime_tab_3(v, i);
+        
+        if(v[i] == 2)
+            printf("\t%d.", i+1);
+        else
+            printf("\t  ");
+            
+        if(i % 3 == 2)
+                printf("\n");
+    }
+    return;
+}
+
+void adiciona_tab(int v[], int jogador)// 0 (jogador 1) X ; 1 (jogador 2) O
+{
+    int i;
+    
+    imprime_tab_opcoes(v);
+    
+    do{
+        if(jogador == 0)
+         printf("jogador 1 (O): ");
+        else
+            printf("jogador 2 (X): ");
+        
+        scanf("%d", &i);
+        i--;
+    }while(0 > i || i > 8 || v[i] != 2); // 0 < i < 9; i nao repetido
+    
+    v[i] = jogador;
+    
+    return;
+}
+
+void reinicia(int v[])
+{
+    for(int i = 0; i <= 8; i++)
+        v[i] = 2;
+    return;
 }
 
 int main ()
 {
-	int jogo[2,2,2,2,2,2,2,2,2];//v[8]
-	int i, rodada, velha;
+	int jogo[8];//[2,2,2,2,2,2,2,2]
+	int i, rodada;
 
 	while(1)//repete a cada vez que o jogo rodar
 	{
 		//escolhe se quer jogar
-		printf("você quer jogar?\n\t1.sim\t2.não\n");
+		printf("voce quer jogar?\n\t1.sim\t2.não\n");
 		do{	scanf("%d", &i);
-		}while(i != 1 || i != 2);
+		}while(i != 1 && i != 2);
 		if(i == 2)//se nao, acaba o laco
 			return 0;
-
-		for(rodada = 1; rodada <= 9; rodada++)//sao 9 rodadas (repeticoes)
+        
+        reinicia(jogo);
+        
+		for(rodada = 0; rodada <= 8; rodada++)//sao 9 rodadas (repeticoes)
 		{
-			imprime_tab();
+			imprime_tab(jogo);
+			printf("\n");
+			
+			adiciona_tab(jogo, rodada % 2);//resto pode ser 0 (jogador 1) ou 1 (jogador 2)
+			printf("\n");
+			
 			if( venceu(jogo) )
 			{
-				printf("GANHOU!!\n\n");
+				printf("\t------GANHOU!!------\n\n");
 				rodada = 9;
 			}
-			else
-			{
-				adiciona_tab(jogo, rodada % 2);//resto pode ser 0 (jogador 1) ou 1 (jogador 2)
-			}
 		}
-		if( !venceu(v) )
-			printf("VELHA!! o jogo acabou!\n\n");
-		//aqui tem que reiniciar o tabuleiro...
+		if( !venceu(jogo) )
+			printf("------VELHA!! o jogo acabou!------\n\n");
+		imprime_tab(jogo);
 	}
 }
